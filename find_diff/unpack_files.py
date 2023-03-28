7aa8c5813d170abf7f83af0996f2e028ab56190d
Emil Murzin <emil.morua@gmail.com>
2023-03-28 19:01:29 +0300

    update unpack_files.py

---
+++ find_diff/unpack_files.py
@@ -0,0 +1,33 @@
+import json
+import os
+import yaml
+
+
+def get_file_format(path: str) -> str:
+    """Get the file format of the specified file."""
+
+    _, ext = os.path.splitext(path)
+    if ext == '.json':
+        return 'json'
+    elif ext in ('.yaml', '.yml'):
+        return 'yaml'
+    else:
+        raise ValueError(f"Unrecognized file format for file {path}")
+
+
+def unpack_file(file_path: str) -> dict:
+    """Read and parse the content of a file in JSON or YAML format."""
+
+    with open(file_path) as file:
+        try:
+            format = get_file_format(file_path)
+            if format == 'json':
+                content = json.load(file)
+            elif format == 'yaml':
+                content = yaml.safe_load(file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"{file_path} does not exist")
+        except (json.JSONDecodeError, yaml.YAMLError):
+            raise ValueError(f"{file_path} is not a valid file format")
+
+        return content
