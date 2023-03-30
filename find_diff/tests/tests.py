35b7f49f2bfd7de23ec778a62cf7d6a57c24f555
Emil Murzin <emil.morua@gmail.com>
2023-03-30 19:20:16 +0300

    update tests.py

--- find_diff/tests/tests.py
+++ find_diff/tests/tests.py
@@ -1,6 +1,4 @@
 import pytest
-import os
-import json
 from find_diff.gendiff import generate_diff
 from find_diff.unpack_files import get_file_format, unpack_file
 from find_diff.formaters.formater_json import get_diff_json
@@ -10,16 +8,73 @@ from find_diff.comparison import compare_dicts
 
 
 TEST_FILES = {
-    'json': ['tests/fixtures/1.json', 'tests/fixtures/2.json',
-             'tests/fixtures/3.json', 'tests/fixtures/4.json'],
-    'yaml': ['tests/fixtures/1.yaml', 'tests/fixtures/2.yaml',
-             'tests/fixtures/3.yaml', 'tests/fixtures/4.yaml']
+    'json': ['tests/fixtures/file1.json',
+             'tests/fixtures/file2.json'],
+    'yaml': ['tests/fixtures/file1.yaml',
+             'tests/fixtures/file2.yml'],
+    'invalid': ['tests/fixtures/invalid_file.txt'],
+    'non-existent': ['tests/fixtures/non_existent.txt']
 }
 
 
-TEST_FORMATS = {'stylish': 'tests/fixtures/correct.txt',
-                'plain': 'tests/fixtures/correct_plain.txt',
-                'json': 'tests/fixtures/correct_json.txt'}
+TEST_DICTS = {
+    'dict1': 'find_diff/tests/fixtures/file1.json',
+    'dict2': 'find_diff/tests/fixtures/file2.json',
+    'dict3': 'find_diff/tests/fixtures/file3.json',
+    'dict4': 'find_diff/tests/fixtures/file4.json',
+    'empty dict': 'find_diff/tests/fixtures/file5.json',
+    'diff_dict': 'find_diff/tests/fixtures/diff_dict.txt',
+    'not_diff_dict': 'find_diff/tests/fixtures/not_diff_dict.txt'
+}
+
+
+EXPECTED_VALUES = {
+    'unpack_js': 'find_diff/tests/fixtures/'
+                 'expected_value/unpack_js.txt',
+    'unpack_yaml': 'find_diff/tests/fixtures/'
+                   'expected_value/unpack_yaml.txt',
+    'compare_empty_dict': 'find_diff/tests/fixtures/'
+                          'expected_value/compare_empty_dict.txt',
+    'compare_empty_from_dict': 'find_diff/tests/fixtures/expected'
+                               '_value/compare_empty_from_dict.txt',
+    'compare_same_dicts': 'find_diff/tests/fixtures/'
+                          'expected_value/compare_same_dicts.txt',
+    'compare_nested_dicts': 'find_diff/tests/fixtures/'
+                            'expected_value/compare_nested_dicts.txt',
+    'compare_flat_dicts': 'find_diff/tests/fixtures/'
+                          'expected_value/compare_flat_dicts.txt',
+    'for_js_empty_dicts': 'find_diff/tests/fixtures/'
+                            'expected_value/for_js_empty_dicts.txt',
+    'for_js_not_diff': 'find_diff/tests/fixtures/'
+                         'expected_value/for_js_not_diff.txt',
+    'for_js_diff_dict': 'find_diff/tests/fixtures/'
+                          'expected_value/for_js_diff_dict.txt',
+    'for_plain_empty_dicts': 'find_diff/tests/fixtures/'
+                            'expected_value/for_plain_empty_dicts.txt',
+    'for_plain_not_diff': 'find_diff/tests/fixtures/'
+                         'expected_value/for_plain_not_diff.txt',
+    'for_plain_diff_dict': 'find_diff/tests/fixtures/'
+                          'expected_value/for_plain_diff_dict.txt',
+    'for_stylish_empty_dicts': 'find_diff/tests/fixtures/'
+                            'expected_value/for_stylish_empty_dicts.txt',
+    'for_stylish_not_diff': 'find_diff/tests/fixtures/'
+                         'expected_value/for_stylish_not_diff.txt',
+    'for_stylish_diff_dict': 'find_diff/tests/fixtures/'
+                          'expected_value/for_stylish_diff_dict.txt',
+    'stylish_format': 'find_diff/tests/fixtures/'
+                      'expected_value/stylish_format.txt',
+    'plain_format': 'find_diff/tests/fixtures/'
+                    'expected_value/plain_format.txt',
+    'json_format': 'find_diff/tests/fixtures/'
+                   'expected_value/json_format.txt',
+    'json_file': 'find_diff/tests/fixtures/'
+                 'expected_value/json_file.txt'
+}
+
+
+@pytest.fixture(params=TEST_DICTS)
+def test_dicts(request):
+    return TEST_DICTS[request.param]
 
 
 @pytest.fixture(params=TEST_FILES)
@@ -27,151 +82,193 @@ def test_files(request):
     return TEST_FILES[request.param]
 
 
-@pytest.fixture(params=TEST_FORMATS)
-def test_format(request):
-    return TEST_FILES[request.param]
+@pytest.fixture(params=EXPECTED_VALUES)
+def expected_value(request):
+    return EXPECTED_VALUES[request.param]
 
 
-def test_get_file_format(test_files):
-    # Test JSON file format
+def read_txt_file(file_path):
+    with open(file_path, "r") as file:
+        contents = file.read()
+
+    return contents
+
+
+def test_get_file_json(test_files):
     assert get_file_format(test_files('json')[0]) == 'json'
 
-    # Test YAML file format
-    assert get_file_format('/path/to/file.yaml') == 'yaml'
-    assert get_file_format('/path/to/file.yml') == 'yaml'
 
-    # Test unrecognized file format
+def test_get_file_yaml(test_files):
+    assert get_file_format(test_files('yaml')[0]) == 'yaml'
+    assert get_file_format(test_files('yaml')[1]) == 'yml'
+
+
+def test_get_file_error(test_files):
     with pytest.raises(ValueError):
-        get_file_format('/path/to/file.txt')
+        get_file_format(test_files('invalid')[0])
 
 
-JSON_PATH = '/path/to/test.json'
-YAML_PATH = '/path/to/test.yaml'
-INVALID_PATH = '/path/to/test.txt'
-NONEXISTENT_PATH = '/path/to/nonexistent/file'
+def test_unpack_file_js(test_files, expected_value):
+    expected_value = read_txt_file(expected_value['unpack_js'])
 
-def test_unpack_file():
-    # Test JSON file format
-    assert unpack_file(JSON_PATH) == {'key': 'value'}
+    assert unpack_file(test_files('json')[0]) == expected_value
 
-    # Test YAML file format
-    assert unpack_file(YAML_PATH) == {'key': 'value'}
 
-    # Test invalid file format
+def test_unpack_file_yml(test_files, expected_value):
+    expected_value = read_txt_file(expected_value['unpack_yaml'])
+
+    assert unpack_file(test_files('yaml')[0]) == expected_value
+
+
+def test_unpack_file_invalid(test_files):
     with pytest.raises(ValueError):
-        unpack_file(INVALID_PATH)
+        unpack_file(test_files('invalid')[0])
 
-    # Test non-existent file
+
+def test_unpack_file_invalid(test_files):
+
     with pytest.raises(FileNotFoundError):
-        unpack_file(NONEXISTENT_PATH)
+        unpack_file(test_files('non-existent')[0])
 
 
-# Test for when both dictionaries are empty
-empty_dict1 = {}
-empty_dict2 = {}
-empty_dict_result = {}
-# Test for when one of the dictionaries is empty
-dict1 = {'key1': 'value1', 'key2': {'nested_key1': 'nested_value1'}}
-dict2 = {}
-empty_dict_from_dict2_result = {'key1': {'dict1': 'value1'}, 'key2': {'dict1': {'nested_key1': 'nested_value1'}}}
-# Test for when both dictionaries are the same
-same_dict1 = {'key1': 'value1', 'key2': {'nested_key1': 'nested_value1'}}
-same_dict2 = {'key1': 'value1', 'key2': {'nested_key1': 'nested_value1'}}
-same_dict_result = {}
-# Test for when one of the dictionaries has nested dictionaries but the other doesn't
-nested_dict1 = {'key1': {'nested_key1': 'nested_value1'}, 'key2': 'value2'}
-nested_dict2 = {'key1': 'value1', 'key2': {'nested_key1': 'nested_value1'}}
-nested_dict_result = {'key1': {'dict1': {'nested_key1': 'nested_value1'}, 'dict2': 'value1'}, 'key2': {'dict1': 'value2', 'dict2': {'nested_key1': 'nested_value1'}}}
-# Test for when both dictionaries have nested dictionaries with different levels of nesting
-diff_nested_dict1 = {'key1': {'nested_key1': 'nested_value1', 'nested_key2': {'nested_key3': 'nested_value3'}}, 'key2': 'value2'}
-diff_nested_dict2 = {'key1': {'nested_key1': 'nested_value1', 'nested_key2': {'nested_key3': 'nested_value4'}}, 'key3': {'nested_key4': 'nested_value5'}}
-diff_nested_dict_result = {'key1': {'nested_key2': {'nested_key3': {'dict1': 'nested_value3', 'dict2': 'nested_value4'}}}, 'key2': {'dict1': 'value2'}, 'key3': {'dict2': {'nested_key4': 'nested_value5'}}}
+def test_compare_dicts_empty_dicts(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['compare_empty_dict'])
+    empty_dict = unpack_file(test_dicts('empty_dict'))
 
-def test_compare_dicts_empty_dicts():
-    assert compare_dicts(empty_dict1, empty_dict2) == empty_dict_result
+    assert compare_dicts(empty_dict, empty_dict) == expected_value
 
-def test_compare_dicts_empty_dict_from_dict2():
-    assert compare_dicts(dict1, empty_dict2) == empty_dict_from_dict2_result
 
-def test_compare_dicts_same_dicts():
-    assert compare_dicts(same_dict1, same_dict2) == same_dict_result
+def test_compare_dicts_empty_dict_from_dict(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['compare_empty_from_dict'])
+    empty_dict = unpack_file(test_dicts('empty_dict'))
+    dic = unpack_file(test_dicts('dict1'))
 
-def test_compare_dicts_nested_dicts_with_missing_keys():
-    assert compare_dicts(nested_dict1, nested_dict2) == nested_dict_result
+    assert compare_dicts(empty_dict, dic) == expected_value
 
-def test_compare_dicts_nested_dicts_with_different_values():
-    assert compare_dicts(diff_nested_dict1, diff_nested_dict2) == diff_nested_dict_result
+
+def test_compare_dicts_same_dicts(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['compare_same_dicts'])
+    dic1 = unpack_file(test_dicts('dict1'))
+    dic2 = unpack_file(test_dicts('dict1'))
+
+    assert compare_dicts(dic1, dic2) == expected_value
+
+
+def test_compare_dicts_nested_dicts(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['compare_nested_dicts'])
+    dic1 = unpack_file(test_dicts('dict3'))
+    dic2 = unpack_file(test_dicts('dict4'))
+
+    assert compare_dicts(dic1, dic2) == expected_value
+
+
+def test_compare_dicts_flat_dicts(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['compare_flat_dicts'])
+    dic1 = unpack_file(test_dicts('dict1'))
+    dic2 = unpack_file(test_dicts('dict2'))
+
+    assert compare_dicts(dic1, dic2) == expected_value
 
 #get_diff_json
 
-def test_get_diff_json_empty_dicts():
-    assert compare_dicts(empty_dict) == empty_dict_result
+def test_get_diff_json_empty_dicts(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_js_empty_dicts'])
+    empty_dict = unpack_file(test_dicts('empty_dict'))
+
+    assert get_diff_json(empty_dict) == expected_value
 
 
-def test_get_diff_json_not_diff():
-    assert compare_dicts(not_diff_dict) == not_diff_result
+def test_get_diff_json_not_diff(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_js_not_diff'])
+    not_diff_dict = read_txt_file(test_dicts('not_diff_dict'))
+
+    assert get_diff_json(not_diff_dict) == expected_value
 
 
-def test_get_diff_json_nested_dict():
-    assert compare_dicts(nested_dict) == nested_dict_result
+def test_get_diff_json_diff_dict(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_js_diff_dict'])
+    diff_dict = read_txt_file(test_dicts('diff_dict'))
 
-
-def test_get_diff_json_flat_dict():
-    assert compare_dicts(flat_dict) == flat_dict_result
+    assert get_diff_json(diff_dict) == expected_value
 
 #get_diff_plain
 
-def test_get_diff_plain_empty_dicts():
-    assert compare_dicts(empty_dict) == empty_dict_result
+def test_get_diff_plain_empty_dicts(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_plain_empty_dicts'])
+    empty_dict = unpack_file(test_dicts('empty_dict'))
+
+    assert get_diff_plain(empty_dict) == expected_value
 
 
-def test_get_diff_plain_not_diff():
-    assert compare_dicts(not_diff_dict) == not_diff_result
+def test_get_diff_plain_not_diff(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_plain_not_diff'])
+    not_diff_dict = read_txt_file(test_dicts('not_diff_dict'))
+
+    assert get_diff_plain(not_diff_dict) == expected_value
 
 
-def test_get_diff_plain_nested_dict():
-    assert compare_dicts(nested_dict) == nested_dict_result
+def test_get_diff_plain_diff_dict(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_plain_diff_dict'])
+    diff_dict = read_txt_file(test_dicts('diff_dict'))
 
-
-def test_get_diff_plain_flat_dict():
-    assert compare_dicts(flat_dict) == flat_dict_result
+    assert get_diff_plain(diff_dict) == expected_value
 
 #get_diff_stylish
 
-def test_get_diff_stylish_empty_dicts():
-    assert compare_dicts(empty_dict) == empty_dict_result
+def test_get_diff_stylish_empty_dicts(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_stylish_empty_dicts'])
+    empty_dict = unpack_file(test_dicts('empty_dict'))
+
+    assert get_diff_stylish(empty_dict) == expected_value
 
 
-def test_get_diff_stylish_not_diff():
-    assert compare_dicts(not_diff_dict) == not_diff_result
+def test_get_diff_stylish_not_diff(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_stylish_not_diff'])
+    not_diff_dict = read_txt_file(test_dicts('not_diff_dict'))
+
+    assert get_diff_stylish(not_diff_dict) == expected_value
 
 
-def test_get_diff_stylish_nested_dict():
-    assert compare_dicts(nested_dict) == nested_dict_result
+def test_get_diff_stylish_diff_dict(test_dicts, expected_value):
+    expected_value = read_txt_file(expected_value['for_stylish_diff_dict'])
+    diff_dict = read_txt_file(test_dicts('diff_dict'))
+
+    assert get_diff_stylish(diff_dict) == expected_value
 
 
-def test_get_diff_stylish_flat_dict():
-    assert compare_dicts(flat_dict) == flat_dict_result
+def test_generate_diff_stylish_format(test_files, expected_value):
+    expected_value = read_txt_file(expected_value['stylish_format'])
+    file1 = unpack_file(test_dicts(test_files('json')[0]))
+    file2 = unpack_file(test_dicts(test_files('json')[1]))
+
+    assert generate_diff(file1, file2) == expected_value
 
 
-#gendiff
+def test_generate_diff_plain_format(test_files, expected_value):
+    expected_value = read_txt_file(expected_value['plain_format'])
+    file1 = unpack_file(test_dicts(test_files('json')[0]))
+    file2 = unpack_file(test_dicts(test_files('json')[1]))
 
-def test_generate_diff_with_stylish_format():
-    expected_output = '    common:\n      setting1: Value 1\n+     setting2: 200\n      setting3:\n        key: value\n-     setting4: blah blah\n    group1:\n      abc: 12345\n+     def: {\n+         ghi: {\n+           jkl: 123\n+         }\n+       }\n    group2:\n-     some_key: some_value\n+     some_key: {\n+       nested: {\n+         value: 5\n+       }\n+     }\n'
-    assert generate_diff(file_path1, file_path2) == expected_output
+    assert generate_diff(file1, file2, 'plain') == expected_value
 
-def test_generate_diff_with_plain_format():
-    expected_output = 'Property common.setting1 was updated. From Value 1 to Value 2\nProperty common.setting4 was removed\nProperty common.setting2 was added with value: 200\nProperty group1.abc was updated. From 12345 to 54321\nProperty group2.some_key was updated. From some_value to {\n  nested: {\n    value: 5\n  }\n}\n'
-    assert generate_diff(file_path1, file_path2, 'plain') == expected_output
 
-def test_generate_diff_with_json_format():
-    expected_output = '{"common": {"setting1": "Value 1", "setting2": 200, "setting3": {"key": "value"}, "setting4": null}, "group1": {"abc": 12345, "def": {"ghi": {"jkl": 123}}}, "group2": {"some_key": {"nested": {"value": 5}}}}'
-    assert generate_diff(file_path1, file_path2, 'json') == expected_output
+def test_generate_diff_json_format(test_files, expected_value):
+    expected_value = read_txt_file(expected_value['json_format'])
+    file1 = unpack_file(test_dicts(test_files('json')[0]))
+    file2 = unpack_file(test_dicts(test_files('json')[1]))
 
-def test_generate_diff_with_unsupported_format():
+    assert generate_diff(file1, file2, 'json') == expected_value
+
+def test_generate_diff_yml_file(test_files, expected_value):
+    expected_value = read_txt_file(expected_value['json_file'])
+    file1 = unpack_file(test_dicts(test_files('yaml')[0]))
+    file2 = unpack_file(test_dicts(test_files('yaml')[1]))
+
+    assert generate_diff(file1, file2, 'json') == expected_value
+
+
+def test_generate_diff_with_unsupported_format(test_files):
+    file1 = unpack_file(test_dicts(test_files('json')[0]))
+    file2 = unpack_file(test_dicts(test_files('json')[1]))
     with pytest.raises(ValueError):
-        generate_diff(file_path1, file_path2, 'unsupported_format')
-
-def test_generate_diff_with_different_file_types():
-    with pytest.raises(TypeError):
-        generate_diff(file_path1, file_path3)
+        generate_diff(file1, file2, 'unsupported_format')
