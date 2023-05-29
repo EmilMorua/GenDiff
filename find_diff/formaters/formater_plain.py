c1249a1264cb36aff4cdb6931de89b3101795e91
Emil Murzin <emil.morua@gmail.com>
2023-05-29 17:58:37 +0300

    update formater_plain.py

--- find_diff/formaters/formater_plain.py
+++ find_diff/formaters/formater_plain.py
@@ -6,48 +6,46 @@ def get_diff_plain(diff_dict: dict) -> str:
     Returns a string representation of the differences
     between two dictionaries.
     """
-
     diff_dict = convert_dict_values(diff_dict)
+    diff_list = []
+    process_dict(diff_dict, diff_list)
+    return ''.join(diff_list)[:-1]
 
-    def process_dict(diff_dict, path=''):
-        diff_list = []
 
-        for key, value in diff_dict.items():
-            current_path = f'{path}.{key}' if path else key
-            if isinstance(value, dict) \
-                    and not {'dict1', 'dict2', 'both'} & set(value.keys()):
-                sub_diff = process_dict(value, current_path)
-                if sub_diff:
-                    diff_list.append(sub_diff)
-            elif 'dict1' in value and 'dict2' in value:
-                if isinstance(value['dict1'], dict) \
-                            and isinstance(value['dict2'], dict):
-                    sub_diff = process_dict(value['dict1'], current_path)
-                    if sub_diff:
-                        diff_list.append(sub_diff)
-                    sub_diff = process_dict(value['dict2'], current_path)
-                    if sub_diff:
-                        diff_list.append(sub_diff)
-                elif value['dict1'] != value['dict2']:
-                    if isinstance(value['dict1'], str) \
-                            and isinstance(value['dict2'], str):
-                        diff_list.append(
-                            f'Property {current_path} was updated. '
-                            f"From '{value['dict1']}' to '{value['dict2']}'\n")
-                    else:
-                        diff_list.append(
-                            f'Property {current_path} was updated. '
-                            f"From [complex value] to '{value['dict2']}'\n")
-            elif 'dict1' in value:
-                diff_list.append(f'Property {current_path} was removed\n')
-            elif 'dict2' in value:
-                if isinstance(value['dict2'], dict):
-                    diff_list.append(f"Property {current_path} was "
-                                     f"added with value: [complex value]\n")
-                else:
-                    diff_list.append(f"Property {current_path} was "
-                                     f"added with value: '{value['dict2']}'\n")
+def process_dict(diff_dict, diff_list, path=''):
+    for key, value in diff_dict.items():
+        current_path = f'{path}.{key}' if path else key
+        if isinstance(value, dict) and not {'dict1', 'dict2', 'both'} \
+                & set(value.keys()):
+            process_dict(value, diff_list, current_path)
+        elif 'dict1' in value and 'dict2' in value:
+            process_updated_value(value, diff_list, current_path)
+        elif 'dict1' in value:
+            process_removed_value(diff_list, current_path)
+        elif 'dict2' in value:
+            process_added_value(value, diff_list, current_path)
 
-        return ''.join(diff_list)
 
-    return process_dict(diff_dict)[:-1]
+def process_updated_value(value, diff_list, current_path):
+    dict1_value = value['dict1']
+    dict2_value = value['dict2']
+    if isinstance(dict1_value, str) and isinstance(dict2_value, str):
+        update_text = (f"From '{dict1_value}' to '{dict2_value}'")
+    else:
+        update_text = (f"From [complex value] to '{dict2_value}'")
+    diff_list.append(f"Property {current_path} was updated. {update_text}\n")
+
+
+def process_removed_value(diff_list, current_path):
+    diff_list.append(f"Property {current_path} was removed\n")
+
+
+def process_added_value(value, diff_list, current_path):
+    dict2_value = value['dict2']
+    if isinstance(dict2_value, dict):
+        diff_list.append(
+            f"Property {current_path} was added with value: [complex value]\n")
+    else:
+        diff_list.append(
+            f"Property {current_path} was added with value: '{dict2_value}'\n"
+            )
