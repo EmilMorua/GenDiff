cc5acf180c364f54ba677b3d6ae2c16bcb56e247
Emil Murzin <emil.morua@gmail.com>
2023-05-30 18:11:14 +0300

    update formater_stylish.py

--- find_diff/formaters/formater_stylish.py
+++ find_diff/formaters/formater_stylish.py
@@ -26,29 +26,43 @@ def get_diff_stylish(diff_dict: dict) -> str:
 
     diff_dict = convert_dict_values(diff_dict)
 
-    def build_diff_string(diff_dict: dict, depth: int = 1, indent: int = 4) -> str:
+    def build_diff_string(
+            diff_dict: dict, depth: int = 1, indent: int = 4) -> str:
         diff_string = ""
 
         for key, values in diff_dict.items():
             nested_indent = " " * (indent * depth)
 
-            if isinstance(values, dict) and not {DICT1, DICT2, BOTH} & set(values):
+            if isinstance(values, dict) and \
+                    not {DICT1, DICT2, BOTH} & set(values):
                 nested_diff = build_diff_string(values, depth + 1, indent)
                 if nested_diff:
-                    diff_string += f"{nested_indent}{key}: {{\n{nested_diff}{nested_indent}}}\n"
+                    diff_string += (
+                        f"{nested_indent}{key}: "
+                        f"{{\n{nested_diff}{nested_indent}}}\n"
+                    )
                 continue
-
             for prefix, dict_key in [("-", DICT1), ("+", DICT2)]:
                 if dict_key in values and isinstance(values[dict_key], dict):
-                    nested_diff = dic_to_string(values[dict_key], depth, indent)
-                    diff_string += f"{nested_indent[2:]}{prefix} {key}: {nested_diff}\n"
+                    nested_diff = dic_to_string(
+                        values[dict_key], depth, indent
+                    )
+                    diff_string += (
+                        f"{nested_indent[2:]}{prefix} "
+                        f"{key}: {nested_diff}\n"
+                    )
                 elif dict_key in values:
-                    diff_string += f"{nested_indent[2:]}{prefix} {key}: {values[dict_key]}\n"
-
+                    diff_string += (
+                        f"{nested_indent[2:]}{prefix} "
+                        f"{key}: {values[dict_key]}\n"
+                    )
             if BOTH in values and isinstance(values[BOTH], dict):
                 nested_diff = dic_to_string(values[BOTH], depth, indent)
                 if ":" in nested_diff:
-                    diff_string += f"{nested_indent} {key}: {{\n{nested_diff}{nested_indent}}}\n"
+                    diff_string += (
+                        f"{nested_indent} {key}: {{\n"
+                        f"{nested_diff}{nested_indent}}}\n"
+                    )
                 else:
                     diff_string += f"{nested_indent} {key}: {nested_diff}\n"
             elif BOTH in values:
