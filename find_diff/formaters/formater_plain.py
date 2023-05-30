b1013466370961d3c8ab61152a15c79dc9f89e1d
Emil Murzin <emil.morua@gmail.com>
2023-05-30 17:48:38 +0300

    update formater_plain.py

--- find_diff/formaters/formater_plain.py
+++ find_diff/formaters/formater_plain.py
@@ -1,4 +1,5 @@
 from find_diff.convert_bool import convert_dict_values
+from find_diff.comparison import DICT1, DICT2, BOTH
 
 
 def get_diff_plain(diff_dict: dict) -> str:
@@ -15,20 +16,20 @@ def get_diff_plain(diff_dict: dict) -> str:
 def process_dict(diff_dict, diff_list, path=''):
     for key, value in diff_dict.items():
         current_path = f'{path}.{key}' if path else key
-        if isinstance(value, dict) and not {'dict1', 'dict2', 'both'} \
+        if isinstance(value, dict) and not {DICT1, DICT2, BOTH} \
                 & set(value.keys()):
             process_dict(value, diff_list, current_path)
-        elif 'dict1' in value and 'dict2' in value:
+        elif DICT1 in value and DICT2 in value:
             process_updated_value(value, diff_list, current_path)
-        elif 'dict1' in value:
+        elif DICT1 in value:
             process_removed_value(diff_list, current_path)
-        elif 'dict2' in value:
+        elif DICT2 in value:
             process_added_value(value, diff_list, current_path)
 
 
 def process_updated_value(value, diff_list, current_path):
-    dict1_value = value['dict1']
-    dict2_value = value['dict2']
+    dict1_value = value[DICT1]
+    dict2_value = value[DICT2]
     if isinstance(dict1_value, str) and isinstance(dict2_value, str):
         update_text = (f"From '{dict1_value}' to '{dict2_value}'")
     else:
@@ -41,7 +42,7 @@ def process_removed_value(diff_list, current_path):
 
 
 def process_added_value(value, diff_list, current_path):
-    dict2_value = value['dict2']
+    dict2_value = value[DICT2]
     if isinstance(dict2_value, dict):
         diff_list.append(
             f"Property {current_path} was added with value: [complex value]\n")
