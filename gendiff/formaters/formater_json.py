d273b0da741f7235fdaa741df37f178e0ec6d5f9
Emil Murzin <emil.morua@gmail.com>
2023-06-21 16:25:11 +0300

    update formater_json.py

--- gendiff/formaters/formater_json.py
+++ gendiff/formaters/formater_json.py
@@ -8,44 +8,52 @@ def get_diff_json(diff_dict):
     Recursively generates a list of differences
     between two dictionaries in JSON format.
     """
+    def get_diff(diff_dict):
+        result = []
+        for key, value in diff_dict.items():
+            if isinstance(value, dict) and \
+                    not {DICT1, DICT2, BOTH} & set(value.keys()):
+                result.append({
+                    KEY: key,
+                    TYPE: "hasChildren",
+                    CHILDREN: get_diff(value)
+                })
+            elif DICT1 in value and DICT2 in value:
+                before_value = value[DICT1]
+                after_value = value[DICT2]
+                result.append({
+                    KEY: key,
+                    TYPE: "changed",
+                    BEFORE_VALUE: before_value,
+                    AFTER_VALUE: after_value
+                })
+            elif DICT1 in value:
+                result.append({
+                    KEY: key,
+                    TYPE: "deleted",
+                    VALUE: value[DICT1]
+                })
+            elif DICT2 in value:
+                result.append({
+                    KEY: key,
+                    TYPE: "added",
+                    VALUE: value[DICT2]
+                })
+            elif BOTH in value:
+                result.append({
+                    KEY: key,
+                    TYPE: "unchanged",
+                    VALUE: value[BOTH]
+                })
 
-    result = []
-    for key, value in diff_dict.items():
-        if isinstance(value, dict) and \
-                not {DICT1, DICT2, BOTH} & set(value.keys()):
-            result.append({
-                KEY: key,
-                TYPE: "hasChildren",
-                CHILDREN: get_diff_json(value)
-            })
-        elif DICT1 in value and DICT2 in value:
-            before_value = value[DICT1]
-            after_value = value[DICT2]
-            result.append({
-                KEY: key,
-                TYPE: "changed",
-                BEFORE_VALUE: before_value,
-                AFTER_VALUE: after_value
-            })
-        elif DICT1 in value:
-            result.append({
-                KEY: key,
-                TYPE: "deleted",
-                VALUE: value[DICT1]
-            })
-        elif DICT2 in value:
-            result.append({
-                KEY: key,
-                TYPE: "added",
-                VALUE: value[DICT2]
-            })
-        elif BOTH in value:
-            result.append({
-                KEY: key,
-                TYPE: "unchanged",
-                VALUE: value[BOTH]
-            })
+        return result
 
-    result = json.dumps(result, ensure_ascii=False, indent=None)
-    result = result.replace("\\", "")
+    def convert_to_string(diff_dict):
+        result = json.dumps(diff_dict, ensure_ascii=False, indent=None)
+        result = result.replace("\\", "")
+        return result
+
+    result = get_diff(diff_dict)
+    result = convert_to_string(result)
+
     return result
