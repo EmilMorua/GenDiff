40260c1eee78bb4e332d13ef0568643cbd9b1f3d
Emil Murzin <emil.morua@gmail.com>
2023-07-10 11:26:55 +0500

    update unpack_files.py

--- gendiff/unpack_files.py
+++ gendiff/unpack_files.py
@@ -15,19 +15,38 @@ def get_file_format(path: str) -> str:
         raise ValueError(f"Unrecognized file format for file {path}")
 
 
+def read_file(file_path: str) -> str:
+    """Read the content of a file."""
+    with open(file_path) as file:
+        content = file.read()
+    return content
+
+
+def parse_content(content: str, format: str) -> dict:
+    """Parse the content of a file in JSON or YAML format."""
+    if format == 'json':
+        try:
+            parsed_content = json.loads(content)
+        except json.JSONDecodeError:
+            raise ValueError("Invalid JSON format")
+    elif format == 'yaml':
+        try:
+            parsed_content = yaml.safe_load(content)
+        except yaml.YAMLError:
+            raise ValueError("Invalid YAML format")
+    else:
+        raise ValueError("Invalid file format")
+
+    return parsed_content
+
+
 def unpack_file(file_path: str) -> dict:
     """Read and parse the content of a file in JSON or YAML format."""
+    try:
+        file_format = get_file_format(file_path)
+        file_content = read_file(file_path)
+        parsed_content = parse_content(file_content, file_format)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"{file_path} does not exist")
 
-    with open(file_path) as file:
-        try:
-            format = get_file_format(file_path)
-            if format == 'json':
-                content = json.load(file, parse_constant=lambda x: x)
-            elif format == 'yaml':
-                content = yaml.safe_load(file)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"{file_path} does not exist")
-        except (json.JSONDecodeError, yaml.YAMLError):
-            raise ValueError(f"{file_path} is not a valid file format")
-
-        return content
+    return parsed_content
